2facfd1881f20b0edf06093c328050060b5a35eb
mendelas <kota4610@gmail.com>
2023-02-13 17:10:14 +0900

    first commit

---
+++ sstv.cpp
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdlib.h>//for random
+#include <iostream>//for debug writing
+#include <fstream>//ifstream, ofstream
+#include <string>//useful for reading and writing
+
+
+const int FREQ_VIS_BIT1 = 1100;
+const int FREQ_SYNC = 1200;
+const int FREQ_VIS_BIT0 = 1300;
+const int FREQ_BLACK = 1500;
+const int FREQ_VIS_START = 1900;
+const int FREQ_WHITE = 2300;
+const int FREQ_RANGE = FREQ_WHITE - FREQ_BLACK
+const int FREQ_FSKID_BIT1 = 1900;
+const int FREQ_FSKID_BIT0 = 2100;
+
+const int MSEC_VIS_START = 300;
+const int MSEC_VIS_SYNC = 10;
+const int MSEC_VIS_BIT = 30;
+const int MSEC_FSKID_BIT = 22;
+
+class SSTV{
+    
+}
